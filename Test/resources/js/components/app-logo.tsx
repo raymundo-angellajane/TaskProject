a9032d7908847775bfc0b65c66ed36d79921b979
raymundo-angellajane <angellajaneraymundo05@gmail.com>
2025-09-13 19:15:22 +0800

    change logo/icon

--- Test/resources/js/components/app-logo.tsx
+++ Test/resources/js/components/app-logo.tsx
@@ -1,13 +1,11 @@
-import AppLogoIcon from './app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-12 items-center justify-center rounded-md">
+                <img src="/Tickora-logo.png" alt="Tickora Logo" className="size-7 object-contain" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">Tickora</span>
+            <div className="ml-0.1 grid flex-1 text-left text-sm">
+                <span className="mb-0.1 truncate leading-tight font-semibold">Tickora</span>
             </div>
         </>
     );
