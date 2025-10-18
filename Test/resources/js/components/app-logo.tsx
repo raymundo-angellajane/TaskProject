f97a72d51d8694ea36f7294ab329dfe4324fd34c
raymundo-angellajane <angellajaneraymundo05@gmail.com>
2025-10-18 16:53:17 +0800

    fix logo collapsible

--- Test/resources/js/components/app-logo.tsx
+++ Test/resources/js/components/app-logo.tsx
@@ -1,10 +1,10 @@
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-12 items-center justify-center rounded-md">
-                <img src="/Tickora-logo.png" alt="Tickora Logo" className="size-7 object-contain" />
+            <div className="flex aspect-square size-12 items-center justify-center rounded-md group-data-[collapsible=icon]:size-8">
+                <img src="/Tickora-logo.png" alt="Tickora Logo" className="size-7 object-contain group-data-[collapsible=icon]:size-6" />
             </div>
-            <div className="ml-0.1 grid flex-1 text-left text-sm">
+            <div className="ml-0.1 grid flex-1 text-left text-sm group-data-[collapsible=icon]:hidden">
                 <span className="mb-0.1 truncate leading-tight font-semibold">Tickora</span>
             </div>
         </>
